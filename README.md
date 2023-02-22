71f8e78910f7fbd3829d8e435ac6bd1d4a1c9d31
Blixt-A <annica.blixt@edu.cmeducations.se>
2023-02-22 12:03:32 +0100

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Ballon d'Or Countdown
+As my first personal project in the JavaScript framework course, I have created a countdown timer for this year's Ballon D'or.
+
+# Design
+I have chosen a design that I think fits as I believe and hope that it is the GOAT Messi who will win his eighth Ballons d'Or.
+
+The design is taken/inspired from: https://www.figma.com/community/file/1093536402370138409 - Credit to JIBIN S. JOSEPH
---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+import backgroundPic from './assets/backgroundIMG.png'
+import goatPic from './assets/goat.png'
+import Timer from './assets/Timer'
+
+function App() {
+  const [timerDays, setTimerDays] = useState();
+  const [timerHours, setTimerHours] = useState();
+  const [timerMinutes, setTimerMinutes] = useState();
+  const [timerSeconds, setTimerSeconds] = useState();
+  
+  let interval;
+
+  const startTimer = () => {
+    const countDownDate = new Date('October 16, 2023').getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance / (24 * 60 * 60 * 1000));      
+      const hours = Math.floor((distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60));      
+      const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+      const seconds = Math.floor((distance % (60 * 1000)) / 1000);
+
+      if(distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    });
+  };
+
+  useEffect(() => {
+    startTimer();
+  });
+
+  return (
+    <div className='App'>
+      <section className="mobile-view">
+        <img className='background-pic' src={backgroundPic}/>
+        <div className='first-content-container'>
+          <p>Ballon d'Or</p> <span>2023</span>
+          <img className='pic' src={goatPic}/>
+        </div>
+        <div className="second-content-container">
+          <p><span>The Countdown</span> is on..</p>
+        </div>
+        <Timer 
+        timerDays={timerDays} 
+        timerHours={timerHours} 
+        timerMinutes={timerMinutes} 
+        timerSeconds={timerSeconds}
+        />
+      </section>
+    </div>
+ );
+}
+
+export default App;
+
